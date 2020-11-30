052913bd8a387edfed6f66c8c37af3ae67e993a6
Caleb Hill <calebleehill1993@gmail.com>
2020-11-30 11:52:29 -0700

    deleting files

---
+++ playerstats.modernfloppydisk.com/src/views/PlayerBio.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+<div>
+  <PlayerBio :player="player" />
+</div>
+<div class="github">
+  <a href="https://github.com/BYU-CS-260-Winter-2020/lab-3b-grocery-store-calebleehill1993">Github Classroom</a>
+</div>
+</div>
+</template>
+
+<script>
+import PlayerBio from "../components/PlayerBio.vue"
+export default {
+  name: 'Cart',
+  components: {
+    PlayerBio
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    player() {
+      return this.$root.$data.currentPlayer;
+    },
+    notEmpty() {
+      return this.$root.$data.notEmpty;
+    }
+  },
+  methods: {
+  }
+}
+</script>
